ed6d9c74d51b871776a2f82a69924c83218fd36c
youhong415 <56081287+youhong415@users.noreply.github.com>
2020-05-17 19:49:39 +0800

    Create voc_annotation_BoxCenter_Version.py

---
+++ voc_annotation_BoxCenter_Version.py
@@ -0,0 +1,42 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sun May 17 12:06:15 2020
+
+@author: bbjac
+"""
+
+
+import xml.etree.ElementTree as ET
+from os import getcwd
+
+sets=[('2007', 'train'), ('2007', 'val'), ('2007', 'test')]
+
+classes = ["ball"]
+
+
+def convert_annotation(year, image_id, list_file):
+    in_file = open('VOCdevkit/VOC%s/Annotations/%s.xml'%(year, image_id),encoding="utf-8")
+    tree=ET.parse(in_file)
+    root = tree.getroot()
+
+    for obj in root.iter('object'):
+        difficult = obj.find('difficult').text
+        cls = obj.find('name').text
+        if cls not in classes or int(difficult)==1:
+            continue
+        cls_id = classes.index(cls)
+        xmlbox = obj.find('bndbox')
+        b = (int(xmlbox.find('xmin').text), int(xmlbox.find('ymin').text), int(xmlbox.find('xmax').text), int(xmlbox.find('ymax').text))
+        list_file.write(" "+str(int((b[0]+b[2])/2))+","+str(int((b[1]+b[3])/2)))
+        #list_file.write(" " + ",".join([str(a) for a in b]) + ',' + str(cls_id))
+
+wd = getcwd()
+
+for year, image_set in sets:
+    image_ids = open('VOCdevkit/VOC%s/ImageSets/Main/%s.txt'%(year, image_set),encoding="utf-8").read().strip().split()
+    list_file = open('%s_%s.txt'%(year, image_set), 'w',encoding="utf-8")
+    for image_id in image_ids:
+        list_file.write('%s/VOCdevkit/VOC%s/JPEGImages/%s.jpg'%(wd, year, image_id))
+        convert_annotation(year, image_id, list_file)
+        list_file.write('\n')
+    list_file.close()
